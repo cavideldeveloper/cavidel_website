3ceb97af18e27e14c258a9cdfcc6e4e560d42b8e
Ekpoto Liberty <ekpoto.liberty@gmail.com>
2019-07-03 12:09:53 +0100

    fixed index page

--- resources/views/__includes/features.blade.php
+++ resources/views/__includes/features.blade.php
@@ -13,40 +13,38 @@
 
 <div class="g-bg-color--sky-light">
     <div class="container">
-        <div class="row">
+        <div class="row g-margin-b-20--lg">
             <!-- Plan -->
             <div class="col-md-4 g-margin-b-0--lg">
                 <div class="wow fadeInUp" data-wow-duration=".3" data-wow-delay=".2s">
                     <div class="g-padding-y-20--xs">
-                        <img src="{{asset('img/products/officemate.png')}}" class="g-margin-b-40--xs" width="auto" height="60">
-                        <p>An enterprise resource planning (ERP) solution with core functionality to automate business operations</p>
-                        <ul class="list-unstyled">
-                            <li class="g-font-weight--500"><i class="g-font-size-13--xs g-color--primary g-margin-r-10--xs ti-check"></i>
-                                Employee onboarding
-                            </li>
-                            <li class="g-font-weight--500"><i class="g-font-size-13--xs g-color--primary g-margin-r-10--xs ti-check"></i>
-                                Chat & Messaging
-                            </li>
-
+                        <img src="{{asset('img/products/officemate.png')}}" class="g-margin-b-20--xs" width="auto" height="60">
+                        <p class="g-font-weight--500">An enterprise resource planning (ERP) solution with core functionality to automate business operations</p>
+                        <ul class="list-unstyled g-ul-li-tb-5--xs">
                             <li class="g-font-weight--500"><i class="g-font-size-13--xs g-color--primary g-margin-r-10--xs ti-check"></i>
                                 Billing & Accounting
                             </li>
 
                             <li class="g-font-weight--500"><i class="g-font-size-13--xs g-color--primary g-margin-r-10--xs ti-check"></i>
-                                Event scheduling
+                                Chat & Messaging
                             </li>
 
                             <li class="g-font-weight--500"><i class="g-font-size-13--xs g-color--primary g-margin-r-10--xs ti-check"></i>
                                 Task management
                             </li>
+
                             <li class="g-font-weight--500"><i class="g-font-size-13--xs g-color--primary g-margin-r-10--xs ti-check"></i>
-                                Alerts & Notifications
+                                Documents Management
                             </li>
+
                             <li class="g-font-weight--500"><i class="g-font-size-13--xs g-color--primary g-margin-r-10--xs ti-check"></i>
                                 Office365 integration
                             </li>
-                        </ul>
 
+                            <li class="g-font-weight--500"><i class="g-font-size-13--xs g-color--primary g-margin-r-10--xs ti-check"></i>
+                                Issues Resolution
+                            </li>
+                        </ul>
                     </div>
                 </div>
             </div>
@@ -89,35 +87,169 @@
     </div>
 </div>
 
+<!-- Counter -->
+<div class="js__parallax-window" style="background: url(img/1920x1080/28.jpg) 50% 0 no-repeat fixed;">
+    <div class="container g-text-center--xs g-padding-y-80--xs g-padding-y-125--sm">
+        <div class="g-margin-b-80--xs">
+            <h2 class="g-font-size-40--xs g-font-weight--700 g-font-size-50--sm g-font-size-60--md g-color--white">
+                Trading Investment & Assets Management Software
+            </h2>
+        </div>
+    </div>
+</div>
+<!-- End Counter -->
+
 <div class="g-bg-color--sky-light">
-    <div class="container g-padding-y-40--xs g-padding-y-100--xsm">
-        <div class="row">
+    <div class="container">
+        <div class="row g-margin-b-20--lg g-margin-t-60--lg">
             <!-- Plan -->
-            <div class="col-md-8 g-margin-b-10--xs g-margin-b-20--lg">
-                <div class="wow fadeInUp" data-wow-duration=".3" data-wow-delay=".1s">
-                    <div class="s-plan-v1 g-text-left--xs g-bg-color--white g-padding-y-5--xs g-padding-x-30--xs">
-                        <p class="g-font-size-14--xs g-color--dark g-margin-b-30--xs g-padding-x-100--xs">
-                            <p>
-                                An enterprise resource planning (ERP) solution with core functionality to automate business operations
-                            </p>
-                            <ul class="list-unstyled g-ul-li-tb-5--xs g-margin-b-40--xs g-text-left--xs">
-                                <li class="g-color--dark g-font-weight--500"> A need to improve performance, but not at the cost of undoing the internal controls and compliance gains of the past few years</li>
-                                <li class="g-color--dark g-font-weight--500"> Globalization drives an increasing number of acquisitions, divestitures and outsourcing of core functionality. This leads to greater operational challenges arising from the associated re-structuring</li>
-                                <li class="g-color--dark g-font-weight--500"> Challenges translating technology investments into significant sustainable business benefits</li>
-                                <li class="g-color--dark g-font-weight--500"> Market demands for shorter product and service development life cycles, better value, and greater security</li>
-                                <li class="g-color--dark g-font-weight--500"> The need to compete globally for market share and scarce resources</li>
-                            </ul>
+            <div class="col-md-4 g-margin-b-0--lg">
+                <div class="wow fadeInUp" data-wow-duration=".3" data-wow-delay=".2s">
+                    <div class="g-padding-y-20--xs">
+                        <img src="{{asset('img/products/tims.png')}}" class="g-margin-b-20--xs" width="auto" height="60">
+                        <p class="g-font-weight--500">Middle and back-office trading & investment management system (TIMS)...
+                            {{-- with capabilities to manage customer’s KYC information, general ledger accounting, customer deposits and withdrawals, loan origination and asset management. --}}
                         </p>
+                        <ul class="list-unstyled g-ul-li-tb-5--xs">
+                            <li class="g-font-weight--500"><i class="g-font-size-13--xs g-color--primary g-margin-r-10--xs ti-check"></i>
+                                Customer onboarding & KYC
+                            </li>
+                            <li class="g-font-weight--500"><i class="g-font-size-13--xs g-color--primary g-margin-r-10--xs ti-check"></i>
+                                General ledger accounting
+                            </li>
+                            <li class="g-font-weight--500"><i class="g-font-size-13--xs g-color--primary g-margin-r-10--xs ti-check"></i>
+                                Auto entries generation and posting
+                            </li>
+                            <li class="g-font-weight--500"><i class="g-font-size-13--xs g-color--primary g-margin-r-10--xs ti-check"></i>
+                                Multi-branch setup
+                            </li>
+                            <li class="g-font-weight--500"><i class="g-font-size-13--xs g-color--primary g-margin-r-10--xs ti-check"></i>
+                                Management report
+                            </li>
+                            <li class="g-font-weight--500"><i class="g-font-size-13--xs g-color--primary g-margin-r-10--xs ti-check"></i>
+                                Request and approval workflow
+                            </li>
+                        </ul>
                     </div>
                 </div>
             </div>
             <!-- End Plan -->
 
             <!-- Plan -->
-            <div class="col-md-4 g-margin-b-10--xs g-margin-b-0--lg">
+            <div class="col-md-8 g-margin-b-0--lg">
+                <div class="container">
+                    <div class="s-swiper js__swiper-testimonials">
+                        <!-- Swiper Wrapper -->
+                        <div class="swiper-wrapper">
+                            <div class="swiper-slide">
+                                <div class="s-plan-v1 g-padding-x-20--xs">
+                                    <div class="g-margin-b-40--xs" style="background: url(img/920x413/03.png) no-repeat; height: 400px;">
+                                    </div>
+                                    <div class="center-block g-width-100--xs g-margin-b-30--xs"></div>
+                                </div>
+                            </div>
+                            <div class="swiper-slide">
+                                <div class="s-plan-v1 g-padding-x-20--xs">
+                                    <div class="g-margin-b-40--xs" style="background: url(img/920x413/02.png) no-repeat; height: 400px;">
+                                    </div>
+                                    <div class="center-block g-width-100--xs g-margin-b-30--xs"></div>
+                                </div>
+                            </div>
+                            <div class="swiper-slide">
+                                <div class="s-plan-v1 g-padding-x-20--xs">
+                                    <div class="g-margin-b-40--xs" style="background: url(img/920x413/01.png) no-repeat; height: 400px;">
+                                    </div>
+                                    <div class="center-block g-width-100--xs g-margin-b-30--xs"></div>
+                                </div>
+                            </div>
+                        </div>
+                        <!-- End Swipper Wrapper -->
+                    </div>
+                </div>
+            </div>
+            <!-- End Plan -->
+        </div>
+    </div>
+</div>
+
+<!-- Counter -->
+<div class="js__parallax-window" style="background: url(img/1920x1080/28.jpg) 50% 0 no-repeat fixed;">
+    <div class="container g-text-center--xs g-padding-y-80--xs g-padding-y-125--sm">
+        <div class="g-margin-b-40--xs">
+            <h2 class="g-font-size-40--xs g-font-weight--700 g-font-size-50--sm g-font-size-60--md g-color--white">
+                We build trading and simulation software
+            </h2>
+        </div>
+    </div>
+</div>
+<!-- End Counter -->
+
+<div class="g-bg-color--sky-light">
+    <div class="container">
+        <div class="row g-margin-b-20--lg g-margin-t-60--lg">
+            <!-- Plan -->
+            <div class="col-md-4 g-margin-b-0--lg">
                 <div class="wow fadeInUp" data-wow-duration=".3" data-wow-delay=".2s">
-                    <div class="s-plan-v1 g-text-center--xs g-bg-color--white g-padding-y-20--xs">
-                        <img src="{{asset('img/products/tims.png')}}" width="auto" height="80">
+                    <div class="g-padding-y-20--xs">
+                        <img src="{{asset('img/products/catss.png')}}" class="g-margin-b-20--xs" width="auto" height="60">
+                        <p class="g-font-weight--500">
+                            The Cavidel Automated Trading & Simulation software features a self-service functionalities that can enable individuals or groups trade virtual financial market instruments.
+                        </p>
+                        <ul class="list-unstyled g-ul-li-tb-5--xs">
+                            <li class="g-font-weight--500"><i class="g-font-size-13--xs g-color--primary g-margin-r-10--xs ti-check"></i>
+                                Mark-to-market valuation
+                            </li>
+
+                            <li class="g-font-weight--500"><i class="g-font-size-13--xs g-color--primary g-margin-r-10--xs ti-check"></i>
+                                Trading position tracking
+                            </li>
+
+                            <li class="g-font-weight--500"><i class="g-font-size-13--xs g-color--primary g-margin-r-10--xs ti-check"></i>
+                                Online real-time trade profitability
+                            </li>
+
+                            <li class="g-font-weight--500"><i class="g-font-size-13--xs g-color--primary g-margin-r-10--xs ti-check"></i>
+                                Market closing price display
+                            </li>
+
+                            <li class="g-font-weight--500"><i class="g-font-size-13--xs g-color--primary g-margin-r-10--xs ti-check"></i>
+                                Leader board
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <!-- End Plan -->
+
+            <!-- Plan -->
+            <div class="col-md-8 g-margin-b-0--lg">
+                <div class="container">
+                    <div class="s-swiper js__swiper-testimonials">
+                        <!-- Swiper Wrapper -->
+                        <div class="swiper-wrapper">
+                            <div class="swiper-slide">
+                                <div class="s-plan-v1 g-padding-x-20--xs">
+                                    <div class="g-margin-b-40--xs" style="background: url(img/920x413/04.png) no-repeat; height: 400px;">
+                                    </div>
+                                    <div class="center-block g-width-100--xs g-margin-b-30--xs"></div>
+                                </div>
+                            </div>
+                            <div class="swiper-slide">
+                                <div class="s-plan-v1 g-padding-x-20--xs">
+                                    <div class="g-margin-b-40--xs" style="background: url(img/920x413/05.png) no-repeat; height: 400px;">
+                                    </div>
+                                    <div class="center-block g-width-100--xs g-margin-b-30--xs"></div>
+                                </div>
+                            </div>
+                            <div class="swiper-slide">
+                                <div class="s-plan-v1 g-padding-x-20--xs">
+                                    <div class="g-margin-b-40--xs" style="background: url(img/920x413/06.png) no-repeat; height: 400px;">
+                                    </div>
+                                    <div class="center-block g-width-100--xs g-margin-b-30--xs"></div>
+                                </div>
+                            </div>
+                        </div>
+                        <!-- End Swipper Wrapper -->
                     </div>
                 </div>
             </div>
--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -13,7 +13,7 @@
     <div class="js__parallax-window" style="background: url(img/1920x1080/27.jpg) 50% 0 no-repeat fixed; height: 290px;">
         <div class="g-container--md g-text-center--xs g-padding-y-150--xs">
             <div class="g-margin-b-60--xs">
-                <h1 class="g-font-size-40--xs g-font-size-50--sm g-font-size-70--md g-color--white g-letter-spacing--1 g-margin-b-30--xs g-font-weight--700 animated bounceInDown">About Us</h1>
+                <h1 class="g-font-size-40--xs g-font-size-50--sm g-font-size-70--md g-color--white g-letter-spacing--1 g-margin-b-30--xs g-font-weight--700 animated bounceInDown"> Get To Know Us More</h1>
             </div>
             <a href="#js__scroll-to-section">
                 <span class="s-icon s-icon--md s-icon--white-brd g-radius--circle ti-angle-double-down"></span>
@@ -29,25 +29,12 @@
 	        <div class="col-md-12 g-padding-y-60--sm g-padding-y-80--md s-plan-v1">
 	            {{-- <p class="text-uppercase g-text-center--xs g-font-size-16--xs g-font-weight--700 g-color--primary g-letter-spacing--2 g-margin-b-15--xs g-color--primary">About </p> --}}
                 <p class="text-uppercase g-text-center--xs g-font-size-28--xs g-font-weight--700 g-color--primary g-letter-spacing--2 g-margin-b-15--xs g-color--primary">
-                    Cavidel Limited
+                    About Us
                 </p>
 	            <p class="g-font-size-18--sm g-padding-x-80--md text-justify g-font-weight--500">
-	            	Cavidel Limited is a process automation company that helps businesses to align/re-align 
-	            	their business units and overall strategies with operational processes and information 
-	            	technology to reduce cost, improve productivity and increase efficiency. 
-					We are a dynamic technology Company with focus on financial technologies and 
-					provision of enterprise resource management tools. 
-					We deliver our solution through detailed process review and automation of
-					business and client integration processes. Our products are specially developed for 
-					financial markets, banking, human resources, e-commerce, real estates, agricultural systems etc.
-					Established in 2013, Cavidel Limited has evolved into a dynamic organisation through its 
-					long tradition and experience in working with conglomerates, financial services firms, 
-					consulting practices, public institutions, medical institutions etc.
-					We take great pride in our reputation for consistently delivering quality services. 
-					We use our ingenuity to save our clients time and money, reduce risks, 
-					increase efficiency and maximise sustainable outcomes. 
-					We do this through innovative thinking, international perspectives, 
-					local knowledge and the immense experience and technical know-how of our team.
+	            	Cavidel Limited is a technology and consulting company that helps businesses achieve business advantage using IT to reduce costs, improve productivity and increase efficiency in the delivery of impeccable customer service.  We deliver our solution through detailed process review and automation of business and client integration processes. Our products are specially developed for financial markets, banking, human resources, health, real estate, agricultural systems etc.
+Established in 2013, Cavidel Limited has evolved into a dynamic organisation through its long tradition and experience in working with conglomerates, financial services firms, consulting practices, public institutions, medical institutions etc. We take great pride in our reputation for consistently delivering quality services. 
+
 				</p>
 	        </div>
 	    </div>
@@ -56,21 +43,15 @@
 
 	<!-- Plan -->
     <div class="g-bg-color--sky-light">
-        <div class="container g-padding-y-80--xs g-padding-y-125--xsm">
-            <div class="row g-margin-b-120--lg">
+        <div class="container g-padding-y-80--xs g-padding-y-80--xsm">
+            <div class="row g-margin-b-20--lg">
                 <!-- Plan -->
                 <div class="col-md-4 g-padding-y-40--md g-margin-b-10--xs g-margin-b-10--lg s-plan-v1">
 
                     <p class="text-uppercase g-text-center--xs g-font-size-16--xs g-font-weight--700 g-color--primary g-letter-spacing--2 g-margin-b-15--xs g-color--primary">Our Apporoach to Work</p>
                     <p class="g-font-size-16--sm g-padding-x-30--md text-justify g-font-weight--500">
-                    	Our approach to work is based on our depth of experience, 
-                    	a high level of professionalism, drive for quality and a customer-centred 
-                    	philosophy. Our commitment to collaborative working and track record of 
-                    	successful projects underpin our capacity to deliver excellent services
-                    	to achieve excellent results at all times.
-						{{-- We work hard at relationships with our clients, our partners as well as our stakeholders. 
-						We recognise the importance of inclusion and participation and this allows us to balance all
-						interests in our partnerships with clients, consultants, stakeholders and the communities we work in. --}}
+                    	Our approach to work is anchored on assembling our multi-disciplinary team with dept of experience and expertise, driving a customer centric philosophy, developing sound work plans and facilitating reviews that drives the actualisation of clients’ strategic objectives.
+                        <br /><br />
 					</p>
                 </div>
                 <!-- End Plan -->
@@ -80,13 +61,7 @@
 
                     <p class="text-uppercase g-text-center--xs g-font-size-16--xs g-font-weight--700 g-color--primary g-letter-spacing--2 g-margin-b-15--xs g-color--primary">Why Partner with Us</p>
                     <p class="g-font-size-16--sm g-padding-x-30--md text-justify g-font-weight--500">
-                        Our people have great passion for process improvement to reduce wastages in the workplace. 
-                        Our team consists of experienced, talented young individuals with fresh minds that always 
-                        come up with new and innovative ideas that can help your business in its pursuit for growth.
-                        <br /><br /><br />
-
-                        {{-- Our work experience had centred around continuous process review and automation 
-                        across various industries. --}}
+                        Cavidel Limited’s management is recognised from its work performance with top leading financial organisations in Nigeria and with combined experience of over 38 years in the finance and consulting services. Partner with CAVIDEL Limited to create new business opportunities. Let’s grow business together.
                     </p>
                 </div>
                 <!-- End Plan -->
@@ -95,13 +70,7 @@
                 <div class="col-md-4 g-padding-y-40--md g-margin-b-10--xs g-margin-b-10--lg s-plan-v1">
                     <p class="text-uppercase g-text-center--xs g-font-size-16--xs g-font-weight--700 g-color--primary g-letter-spacing--2 g-margin-b-15--xs g-color--primary">Our People</p>
                     <p class="g-font-size-16--sm g-padding-x-30--md text-justify g-font-weight--500">
-                        With a combined experience of over 38 years in finance and consulting services, 
-                        our people are well informed to provide quality service and support assistance
-                        for any project we are engaged in. Cavidel Limited prides itself with staff that have work
-                        experience that cuts across five different Nigerian banks, international professional
-                        {{-- consulting firms, securities exchange, conglomerates and a real estate companies. 
-                        Our people possess professional certifications to back up their experiences which made 
-                        them well sought after within the industry they worked. --}}
+                        Cavidel Limited prides itself with a team that have work experience that cuts across five different Nigerian banks, international professional consulting firms, securities, conglomerate and real estate companies. Behind Cavidel Limited is a team of highly innovative minds who are excellently supportive to teammates, friendly and organized.
                     </p>
                 </div>
                 <!-- End Plan -->
