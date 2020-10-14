fcebc8f27673d7a2ffce15d720d1e3c588914b15
ijosef <emeruwajoseph@gmail.com>
2020-10-14 17:21:28 +0100

    feat: minor changes

--- resources/views/pages/blog-post.blade.php
+++ resources/views/pages/blog-post.blade.php
@@ -138,36 +138,42 @@
 @section('scripts')
 	<script type="text/javascript">
 		// load modules
-        loadGlobalNews();
+        // loadGlobalNews();
+        fetchPost(id);
 
-        // load global news
-        function loadGlobalNews() {
-            $.get('{{ url('load/global/news') }}', function(data) {
-                $(".display-news").html("");
-                var sn = 0;
-                $.each(data, function(index, val) {
-                    sn++;
-                    $(".display-news").append(`
-                        <div class="col-sm-12 g-margin-b-30--xs g-margin-b-10--md">
-                            <!-- News -->
-                            <article>
-                                <img width="90%" height="260" style="box-shadow:0px 0px 4px 0px #CCC;border-radius:4px;" src="${val.urlToImage}" alt="Image">
-                                <div class="g-bg-color--white g-box-shadow__dark-lightest-v2">
-                                    <p class="text-uppercase g-font-size-14--xs g-font-weight--700 g-color--primary g-letter-spacing--2">${val.title}</p>
-                                    <h3 class="g-font-size-22--xs g-letter-spacing--1"><h3>
-                                    <p>${val.description}</p>
-                                </div>
-                            </article>
-                            <!-- End News -->
-                        </div>
-                    `);
 
-                    if(sn > 4){
-                        // void loop
-                        return false;
-                    }
-                });
-            });
+        //load post
+        function fetchPost(id) {
+            console.log(id);
         }
+        // load global news
+        // function loadGlobalNews() {
+        //     $.get('{{ url('load/global/news') }}', function(data) {
+        //         $(".display-news").html("");
+        //         var sn = 0;
+        //         $.each(data, function(index, val) {
+        //             sn++;
+        //             $(".display-news").append(`
+        //                 <div class="col-sm-12 g-margin-b-30--xs g-margin-b-10--md">
+        //                     <!-- News -->
+        //                     <article>
+        //                         <img width="90%" height="260" style="box-shadow:0px 0px 4px 0px #CCC;border-radius:4px;" src="${val.urlToImage}" alt="Image">
+        //                         <div class="g-bg-color--white g-box-shadow__dark-lightest-v2">
+        //                             <p class="text-uppercase g-font-size-14--xs g-font-weight--700 g-color--primary g-letter-spacing--2">${val.title}</p>
+        //                             <h3 class="g-font-size-22--xs g-letter-spacing--1"><h3>
+        //                             <p>${val.description}</p>
+        //                         </div>
+        //                     </article>
+        //                     <!-- End News -->
+        //                 </div>
+        //             `);
+
+        //             if(sn > 4){
+        //                 // void loop
+        //                 return false;
+        //             }
+        //         });
+        //     });
+        // }
 	</script>
 @endsection
